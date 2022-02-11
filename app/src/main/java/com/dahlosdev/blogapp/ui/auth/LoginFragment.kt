226812230a9ba594f0d732dbf7e5fc1212a6d992
Sebastian Manriquez <seba.manriquezh@gmail.com>
2022-02-11 15:15:17 -0300

    RegisterFragment.kt added and Navigation added

--- app/src/main/java/com/dahlosdev/blogapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/dahlosdev/blogapp/ui/auth/LoginFragment.kt
@@ -30,6 +30,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding = FragmentLoginBinding.bind(view)
         isUserLoggedIn()
         doLogin()
+        goToSignUpPage()
     }
 
     private fun isUserLoggedIn() {
@@ -44,6 +45,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             val password = binding.editTextPassword.text.toString().trim()
             validateCredentials(email, password)
             signIn(email, password)
+        }
+    }
+
+    private fun goToSignUpPage() {
+        binding.textSignup.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
     }
 
