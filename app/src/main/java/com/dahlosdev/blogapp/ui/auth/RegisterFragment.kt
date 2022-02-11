ad4204e09fd16cdef2cb473172778aefdc656bed
Sebastian Manriquez <seba.manriquezh@gmail.com>
2022-02-11 15:43:31 -0300

    main_graph.xml animations transitions added, fragment_register.xml binding inputs

--- app/src/main/java/com/dahlosdev/blogapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/dahlosdev/blogapp/ui/auth/RegisterFragment.kt
@@ -6,10 +6,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dahlosdev.blogapp.R
+import com.dahlosdev.blogapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
+    private lateinit var binding: FragmentRegisterBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterBinding.bind(view)
+        signUp()
+    }
+
+    private fun signUp() {
+
+        binding.btnSignup.setOnClickListener {
+
+            val email = binding.editTextEmail.text.toString().trim()
+            val username = binding.editTextUsername.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+            if (password != confirmPassword) {
+                binding.editTextPassword.error = "Password doesn't not match"
+                binding.editTextConfirmPassword.error = "Password doesn't not match"
+                return@setOnClickListener
+            }
+
+            if (username.isEmpty()) {
+                binding.editTextUsername.error = "Username is empty"
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty()) {
+                binding.editTextEmail.error = "Email is empty"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.editTextPassword.error = "Password is empty"
+                return@setOnClickListener
+            }
+
+            if (confirmPassword.isEmpty()) {
+                binding.editTextConfirmPassword.error = "Confirm Password is empty"
+                return@setOnClickListener
+            }
+
+        }
     }
 }
