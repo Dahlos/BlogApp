93d723cbb86f71bb373061220a319b26043167b5
Sebastian Manriquez <seba.manriquezh@gmail.com>
2022-02-11 00:51:40 -0300

    Presentation Layer

--- app/src/main/java/com/dahlosdev/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/dahlosdev/blogapp/ui/home/HomeScreenFragment.kt
@@ -3,9 +3,17 @@ package com.dahlosdev.blogapp.ui.home
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.dahlosdev.blogapp.R
+import com.dahlosdev.blogapp.core.Resource
 import com.dahlosdev.blogapp.data.model.Post
+import com.dahlosdev.blogapp.data.remote.HomeScreenDataSource
 import com.dahlosdev.blogapp.databinding.FragmentHomeScreenBinding
+import com.dahlosdev.blogapp.domain.HomeScreenRepoImpl
+import com.dahlosdev.blogapp.presentation.HomeScreenViewModel
+import com.dahlosdev.blogapp.presentation.HomeScreenViewModelFactory
 import com.dahlosdev.blogapp.ui.home.adapter.HomeScreenAdapter
 import com.google.firebase.Timestamp
 
@@ -13,32 +21,31 @@ import com.google.firebase.Timestamp
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
     private lateinit var binding: FragmentHomeScreenBinding
+    private val viewModel by viewModels<HomeScreenViewModel> { HomeScreenViewModelFactory(HomeScreenRepoImpl(HomeScreenDataSource())) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
-        val postList = listOf(
-            Post(
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
-                "gaston",
-                Timestamp.now(),
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
-            ),
-            Post(
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
-                "gaston",
-                Timestamp.now(),
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
-            ),
-            Post(
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
-                "gaston",
-                Timestamp.now(),
-                "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
-            )
-        )
+        viewModel.fetchLatestPosts().observe(
+            viewLifecycleOwner, Observer { result ->
+                when (result) {
 
-        binding.rvHome.adapter = HomeScreenAdapter(postList)
+                    is Resource.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+
+                    is Resource.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.rvHome.adapter = HomeScreenAdapter(result.data)
+                    }
+
+                    is Resource.Failure -> {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), "An Error Occur ${result.exception}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        )
     }
 }
